ba14efda5c5d3ed5b78b12a7d5be6e54fb38d033
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-10 02:04:47 +0800

    syle: change to single quote

--- webpack.common.js
+++ webpack.common.js
@@ -1,43 +1,43 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: './src/template.html',
       // favicon: "./src/favicon.ico",
     }),
   ],
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-    assetModuleFilename: "assets/[hash][ext][query]",
+    assetModuleFilename: 'assets/[hash][ext][query]',
   },
   module: {
     rules: [
       {
         // HTML
         test: /\.html$/,
-        use: "html-loader",
+        use: 'html-loader',
       },
       {
         // IMAGES
         test: /\.(png|svg|webp|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         //FONTS
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
