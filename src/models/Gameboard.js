0a680d3bba2412e5794c45dcb1bb082c4591a2e1
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-10 02:08:21 +0800

    feat: add class Board and implement test

--- src/models/Gameboard.js
+++ src/models/Gameboard.js
@@ -1,8 +1,83 @@
+import { Ship } from './Ship.js';
+
 export class Gameboard {
   constructor() {
-    this.matrix = Array.from({ length: 10 }, () => Array(10).fill(0));
+    this.matrix = Array.from({ length: 10 }, () => Array(10).fill(null));
+    this.coordinates = new Set();
+    this.isGameOver = false;
+    this.army = [];
+  }
+
+  placeShip(ship, x, y, orientation) {
+    if (x > 10 || y > 10 || x < 0 || y < 0) return;
+
+    // IF ORIENTATION IS VERTICAL(V)
+    if (orientation === 'V') {
+      // CHECKS SHIP IF IT OVERLAP VERTICALLY FROM THE BOARD. IF TRUE STOP OPERATION
+      if (ship.length + x > 10) return;
+
+      //CHECKS IF THE SHIP WILL OVERLAP TO OTHER SHIPS.IF TRUE STOPS OPERATION
+      for (let i = 0; i < ship.length; i++) {
+        const tmp = this.matrix[x + i][y];
+        if (tmp) return;
+      }
+
+      // PLACE SHIP IF IT FITS IN THE BOARD
+      for (let i = 0; i < ship.length; i++) {
+        this.matrix[x + i][y] = ship;
+      }
+
+      // PUT THE SHIP IN OBJECT(this.army) FOR TRACKING DAMAGE AND IF IT HAS SUNKED
+      this.army.push(ship);
+
+      /*===============================================================================*/
+      // IF ORIENTATION IS HORIZONTALLY(H)
+    } else if (orientation === 'H') {
+      // CHECKS SHIP IF IT OVERLAP HORIZONTALLY FROM THE BOARD. IF TRUE STOP OPERATION
+      if (ship.length + y > 10) return;
+
+      //CHECKS IF THE SHIP WILL OVERLAP TO OTHER SHIPS.IF TRUE STOPS OPERATION
+      for (let i = 0; i < ship.length; i++) {
+        const tmp = this.matrix[x][y + i];
+        if (tmp) return;
+      }
+
+      // PLACE SHIP IF IT FITS IN THE BOARD
+      for (let i = 0; i < ship.length; i++) {
+        this.matrix[x][y + i] = ship;
+      }
+
+      // PUT THE SHIP IN OBJECT(this.army) FOR TRACKING DAMAGE AND IF IT HAS SUNKED
+      this.army.push(ship);
+    }
+  }
+
+  // RECIEVING ATTACK IN BOARD
+  receiveAttack(x, y) {
+    if (x > 9 || y > 9 || x < 0 || y < 0) return;
+
+    // IF this.isGameOver IS TRUE CANCEL THE OPERATION BELOW
+    if (this.isGameOver) return;
+
+    //TRACKS ATTACK MARKS ON THE BOARD TO PREVENT ATTACKING TWICE OR MORE ON THE SAME COORDINATES
+    const tmpCoordinates = `${x},${y}`;
+    if (this.coordinates.has(tmpCoordinates)) return null;
+
+    let shot = this.matrix[x][y];
+
+    //IF THE SHOT DIRECTED IN A SHIP OBJECT RUN THE CODE BELLOW
+    if (shot) {
+      // +1 DAMAGE ON SHIP
+      shot.hit();
+
+      // IF ALL THE SHIP SUNKED DISPLAY GAMEOVER AND CHANGE this.isGameOver to TRUE;
+      if (this.army.every((ship) => ship.isSunk() === true)) {
+        console.log('GameOver');
+        this.isGameOver = true;
+      }
+    }
+
+    // PUT THE SHIP IN OBJECT(this.army) FOR TRACKING DAMAGE AND IF IT HAS SUNKED
+    this.coordinates.add(x + ',' + y);
   }
 }
-
-const game = new Gameboard();
-console.log(game.matrix);
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,17 +1,122 @@
 import { Gameboard } from '../models/Gameboard.js';
-import { Ship } from '../models/Ship.js';
+import { Ship } from '../models/Ship';
 
-describe('Test class Gameboard', () => {
-  it('tests if the ship is at specific  coordinates', () => {
-    const game = new Gameboard();
-    const carrier = new Ship(5);
-    // const battleship = new Ship(4);
-    // const destroyer = new Ship(3);
-    // const submarine = new Ship(3);
-    // const patrol_boat = new Ship(2);
-    for (let i = carrier.length - 1; i >= 0; i--) {
-      game.board[1 + i][0] = 'carrier';
-    }
-    console.log(game.board);
+/*
+Gameboard
+│
+├── placeShip()
+│   ├── places horizontal ship
+│   ├── places vertical ship
+│   ├── prevents ship going outside board
+│   └── prevents overlapping ships
+│
+└── receiveAttack()
+    ├── damages ship when hit
+    ├── records missed attack
+    ├── prevents attacking same coordinate twice
+    └── detects when all ships are sunk
+*/
+describe('Test Battleship Gameboard', () => {
+  describe('Test placeShip() method', () => {
+    it('places horizontal ship', () => {
+      const board = new Gameboard();
+      const carrier = new Ship(5);
+      board.placeShip(carrier, 0, 4, 'H');
+
+      for (let i = 0; i < carrier.length; i++) {
+        expect(board.matrix[0][4 + i]).toBe(carrier);
+      }
+    });
+
+    it('places vertical ship', () => {
+      const board = new Gameboard();
+      const battleship = new Ship(4);
+      board.placeShip(battleship, 0, 0, 'V');
+
+      for (let i = 0; i < battleship.length; i++) {
+        expect(board.matrix[0 + i][0]).toBe(battleship);
+      }
+    });
+
+    it('prevents ship going outside board', () => {
+      const board = new Gameboard();
+      const copy = board.matrix.map((row) => [...row]);
+      const cruiser = new Ship(3);
+
+      // board.placeShip(cruiser, 0, 10, 'V');
+      board.placeShip(cruiser, 0, 8, 'H');
+
+      expect(board.matrix).toEqual(copy);
+    });
+
+    it('prevents overlapping ships', () => {
+      const board = new Gameboard();
+      const battleship = new Ship(4);
+      const cruiser = new Ship(3);
+
+      board.placeShip(cruiser, 5, 5, 'V');
+      const copy = board.matrix.map((row) => [...row]);
+
+      board.placeShip(battleship, 5, 5, 'H');
+
+      expect(board.matrix).toEqual(copy);
+    });
+  });
+
+  describe('Test receiveAttack() method', () => {
+    it('damages ship when hit', () => {
+      const board = new Gameboard();
+      const carrier = new Ship(5);
+      const battleship = new Ship(4);
+
+      board.placeShip(carrier, 0, 4, 'H');
+      board.placeShip(battleship, 6, 0, 'V');
+
+      console.log(board.matrix);
+
+      board.receiveAttack(0, 4);
+      board.receiveAttack(0, 5);
+      board.receiveAttack(7, 0);
+
+      expect(board.army[0].damage).toBe(2);
+      expect(board.army[1].damage).toBe(1);
+    });
+
+    it('prevents attacking same coordinate twice', () => {
+      const board = new Gameboard();
+      const carrier = new Ship(5);
+      const battleship = new Ship(4);
+
+      board.placeShip(carrier, 0, 4, 'H');
+      board.placeShip(battleship, 6, 0, 'V');
+
+      board.receiveAttack(0, 4);
+      board.receiveAttack(0, 5);
+      board.receiveAttack(7, 0);
+
+      expect(board.receiveAttack(7, 0)).toBe(null);
+    });
+
+    it('detects when all ships are sunk', () => {
+      const board = new Gameboard();
+      const carrier = new Ship(5);
+      const battleship = new Ship(4);
+
+      board.placeShip(carrier, 0, 4, 'H');
+      board.placeShip(battleship, 6, 0, 'V');
+
+      board.receiveAttack(0, 4);
+      board.receiveAttack(0, 5);
+      board.receiveAttack(0, 6);
+      board.receiveAttack(0, 7);
+      board.receiveAttack(0, 8);
+
+      board.receiveAttack(6, 0);
+      board.receiveAttack(7, 0);
+      board.receiveAttack(8, 0);
+      board.receiveAttack(9, 0);
+
+      expect(board.isGameOver).toBe(true);
+    });
   });
 });
