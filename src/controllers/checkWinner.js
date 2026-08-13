491ad31348755f662e6ce85fda6a2cefa1ba6c10
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-14 01:39:47 +0800

    feat: checks game status of whos turn it is and checks  who is the winner

--- src/controllers/checkWinner.js
+++ src/controllers/checkWinner.js
@@ -1,13 +1,15 @@
 import { player1, player2 } from './players.js';
 
+const status = document.querySelector('.status');
+
 export function checkWinner() {
   if (player2.gameboard.isGameOver) {
-    console.log('P1 Wins');
+    status.textContent = 'Player 1 Wins';
     return true;
   }
 
   if (player1.gameboard.isGameOver) {
-    console.log('P2 Wins');
+    status.textContent = 'Player 2 Wins';
     return true;
   }
 
--- src/controllers/turn.js
+++ src/controllers/turn.js
@@ -1,5 +1,10 @@
 export let turn = Math.random() < 0.5 ? 'P1' : 'P2';
 
+const status = document.querySelector('.status');
+
+status.textContent = `Player ${turn}'s Turn`;
+
 export function changeTurn() {
   turn = turn === 'P1' ? 'P2' : 'P1';
+  status.textContent = `Player ${turn}'s Turn`;
 }
