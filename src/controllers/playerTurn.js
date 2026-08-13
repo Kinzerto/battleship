2331ade11205166c2ba66aa1efa34f02715202ec
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-13 16:49:23 +0800

    feat: enablePlayerAttacks function adds event listener to every element  and configure if its hit or miss

---
+++ src/controllers/playerTurn.js
@@ -0,0 +1,43 @@
+import { turn } from './turn.js';
+import { changeTurn } from './turn.js';
+import { checkWinner } from './checkWinner.js';
+import { computer } from './computer.js';
+import { P1, player2 } from './players.js';
+
+export function enablePlayerAttacks(P2) {
+  const cells = P2.children;
+
+  for (const cell of cells) {
+    cell.addEventListener('click', () => {
+      if (turn !== 'P1') return;
+      if (checkWinner()) return;
+
+      //grabing the data attr value
+      const row = cell.dataset.row;
+      const column = cell.dataset.column;
+
+      const result = player2.gameboard.receiveAttack(row, column);
+
+      if (result === null) return;
+
+      const attackResult = document.createElement('div');
+
+      if (result === 'hit') {
+        attackResult.classList.add('damaged');
+      } else if (result === 'miss') {
+        attackResult.classList.add('missed');
+      } else {
+        return;
+      }
+
+      cell.appendChild(attackResult);
+
+      if (checkWinner()) return;
+      changeTurn();
+
+      setTimeout(() => {
+        computer(P1);
+      }, 1000);
+    });
+  }
+}
