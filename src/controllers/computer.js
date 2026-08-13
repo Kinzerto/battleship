31cefc4bce4816d7ce76166d1bb0d4af73b7087d
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-13 16:51:16 +0800

    feat: adds computer opponent

---
+++ src/controllers/computer.js
@@ -0,0 +1,33 @@
+import { checkWinner } from './checkWinner.js';
+import { changeTurn } from './turn.js';
+import { player1 } from './players.js';
+
+export function computer(P1) {
+  let result;
+  let row;
+  let column;
+
+  do {
+    row = Math.floor(Math.random() * 10);
+    column = Math.floor(Math.random() * 10);
+
+    result = player1.gameboard.receiveAttack(row, column);
+  } while (result === null);
+
+  const attackResult = document.createElement('div');
+
+  const box = P1.querySelector(`[data-row="${row}"][data-column="${column}"]`);
+
+  if (result === 'hit') {
+    attackResult.classList.add('damaged');
+  } else if (result === 'miss') {
+    attackResult.classList.add('missed');
+  } else {
+    return;
+  }
+
+  box.appendChild(attackResult);
+  if (checkWinner()) return;
+
+  changeTurn();
+}
