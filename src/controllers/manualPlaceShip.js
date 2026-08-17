76124938aa86c27e82c8b64d12f3f945fff25e34
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-17 23:40:47 +0800

    feat: drag and drop ships onto board

--- src/controllers/manualPlaceShip.js
+++ src/controllers/manualPlaceShip.js
@@ -1,73 +1,60 @@
-import { gameState } from '../index.js';
 import { renderShip } from '../render/renderShips.js';
 import { player1, P1 } from './players.js';
 
-let currentLength;
-export function manual() {
-  const shipContainer = document.querySelector('.shipContainer');
+const shipContainer = document.querySelector('.shipContainer');
 
-  player1.ships.forEach((ship) => {
+export function manual() {
+  shipContainer.replaceChildren();
+
+  player1.ships.forEach((ship, index) => {
     const shipEl = document.createElement('div');
     shipEl.ship = ship;
+    shipEl.dataset.shipId = index;
     shipEl.classList.add('ship');
-    shipEl.setAttribute('draggable', 'true');
-    shipEl.setAttribute('data-length', ship.length);
+    shipEl.draggable = true;
+    shipEl.style.width = `${ship.length * 40}px`;
 
-    const width = ship.length * 40;
-    shipEl.style.width = `${width}px`;
+    shipEl.dataset.orientation = 'H';
+
+    shipEl.addEventListener('dblclick', () => {
+      shipEl.dataset.orientation =
+        shipEl.dataset.orientation === 'H' ? 'V' : 'H';
+
+      if (shipEl.dataset.orientation === 'H') {
+        shipEl.style.width = `${ship.length * 40}px`;
+        shipEl.style.height = '40px';
+      } else {
+        shipEl.style.width = '40px';
+        shipEl.style.height = `${ship.length * 40}px`;
+      }
+    });
+
     shipContainer.appendChild(shipEl);
 
     shipEl.addEventListener('dragstart', (e) => {
-      e.dataTransfer.setData('text/plain', shipEl.dataset.length);
-      currentLength = ship.length;
+      e.dataTransfer.setDragImage(shipEl, 10, 20);
+      e.dataTransfer.setData('text/plain', index);
     });
   });
-
-  P1.addEventListener('dragover', (e) => {
-    e.preventDefault();
-  });
-
-  P1.addEventListener('dragenter', (e) => {
-    const el = e.target;
-    console.log(el.dataset.column);
-    for (let i = 0; i < currentLength; i++) {
-      const box = P1.querySelector(
-        `[data-row="${+el.dataset.row}"][data-column="${+el.dataset.column + i}"]`,
-      );
-      if (!box) return;
-      box.classList.add('active-hover');
-    }
-  });
-
-  P1.addEventListener('dragleave', (e) => {
-    const el = e.target;
-
-    for (let i = 0; i < currentLength; i++) {
-      const box = P1.querySelector(
-        `[data-row="${+el.dataset.row}"][data-column="${+el.dataset.column + i}"]`,
-      );
-      if (!box) return;
-      box.classList.remove('active-hover');
-    }
-  });
-
-  P1.addEventListener('drop', (e) => {
-    e.preventDefault();
-    const row = +e.target.dataset.row;
-    const column = +e.target.dataset.column;
-
-    const shipLength = e.dataTransfer.getData('text/plain');
-
-    const el = shipContainer.querySelector(`[data-length="${shipLength}"]`);
-
-    const place = player1.gameboard.placeShip(el.ship, row, column, 'H');
-    if (!place) return;
-
-    renderShip(el.ship.length, row, column, 'H');
-    // if (player1.gameboard.army.length < 5) gameState.inGame = true;
-
-    el.remove();
-
-    console.log(player1.gameboard);
-  });
 }
+
+P1.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+P1.addEventListener('drop', (e) => {
+  e.preventDefault();
+  const row = +e.target.dataset.row;
+  const column = +e.target.dataset.column;
+
+  const shipId = e.dataTransfer.getData('text/plain');
+  const el = shipContainer.querySelector(`[data-ship-id="${shipId}"]`);
+  const orientation = el.dataset.orientation;
+
+  const place = player1.gameboard.placeShip(el.ship, row, column, orientation);
+
+  if (!place) return;
+
+  renderShip(el.ship.length, row, column, orientation);
+  el.remove();
+});
