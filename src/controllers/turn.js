0a351d30fcc3fa462d0e48e6257fffdab7ef767b
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-29 02:56:06 +0800

    refactor: add active class on whos turn to attack to display a border on board

--- src/controllers/turn.js
+++ src/controllers/turn.js
@@ -1,10 +1,28 @@
 import { gameState } from './state.js';
-
-const status = document.querySelector('.status');
+import { P1Element, P2Element, player1, player2 } from './players.js';
+export const status = document.querySelector('.status');
 
 status.textContent = `Player ${gameState.turn}'s Turn`;
 
 export function changeTurn() {
   gameState.turn = gameState.turn === 'P1' ? 'P2' : 'P1';
-  status.textContent = `Player ${gameState.turn}'s Turn`;
+  whosTurn();
+
+  if (!gameState.inGame) return;
+  activeBoard();
+}
+
+export function activeBoard() {
+  if (gameState.turn === 'P1') {
+    P2Element.classList.add('active');
+    P1Element.classList.remove('active');
+  } else if (gameState.turn === 'P2') {
+    P1Element.classList.add('active');
+    P2Element.classList.remove('active');
+  }
+}
+
+export function whosTurn() {
+  const GameName = gameState.turn === 'P1' ? player1.name : player2.name;
+  status.textContent = `${GameName}'s Turn`;
 }
