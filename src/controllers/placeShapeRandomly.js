958db865f5ca8e23cc62f181697f3e8a73e607e6
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-29 02:39:00 +0800

    feat: if ships is placed ramdomly disable drag feat and notify player to press play

--- src/controllers/placeShapeRandomly.js
+++ src/controllers/placeShapeRandomly.js
@@ -1,14 +1,18 @@
 import { renderShip } from '../render/renderPlacedShips.js';
 import { player1 } from './players.js';
 import { reset } from './restart.js';
+import { shipContainer } from './manualPlaceShip.js';
+import { status } from './turn.js';
 
 export function placeShapeRandomly(player) {
   if (player.name !== 'Computer') {
     reset(player1);
   }
+
   const orientations = ['H', 'V'];
 
   if (player.gameboard.army.length >= player.ships.length) return;
+  status.textContent = 'PRESS PLAY TO START';
 
   player.ships.forEach((ship) => {
     let placed = false;
@@ -22,7 +26,13 @@ export function placeShapeRandomly(player) {
       placed = player.gameboard.placeShip(ship, x, y, orientation);
     }
     if (player.name !== 'Computer') {
-      renderShip(ship.length, x, y, orientation);
+      renderShip(ship.length, x, y, orientation, ship.name);
     }
+  });
+
+  const ships = shipContainer.querySelectorAll(`.ship`);
+  ships.forEach((ship) => {
+    ship.draggable = false;
+    ship.classList.add('draggableOff');
   });
 }
