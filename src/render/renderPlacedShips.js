380eafcee594209a838ee968db00f5437b8f837b
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-29 02:57:50 +0800

    style: add class on ships for styling

--- src/render/renderPlacedShips.js
+++ src/render/renderPlacedShips.js
@@ -1,4 +1,4 @@
-export function renderShip(len, x, y, orientation, player) {
+export function renderShip(len, x, y, orientation, shipName) {
   for (let i = 0; i < len; i++) {
     const row = orientation === 'V' ? x + i : x;
     const column = orientation === 'H' ? y + i : y;
@@ -7,6 +7,32 @@ export function renderShip(len, x, y, orientation, player) {
       `.player1 div[data-row="${row}"][data-column="${column}"]`,
     );
 
-    boardElement.style.backgroundColor = 'lightblue';
+    const whole = document.createElement('div');
+    whole.classList.add('ship');
+    whole.classList.add(shipName);
+    whole.classList.add(`box${i}`);
+    boardElement.appendChild(whole);
+
+    if (i === 0 && orientation === 'H') {
+      whole.classList.add('tailHorizontal');
+    } else if (i === len - 1 && orientation === 'H') {
+      whole.classList.add('headHorizontal');
+    } else if (orientation === 'H') {
+      whole.classList.add('horizontalMiddle');
+    }
+
+    if (i === 0 && orientation === 'V') {
+      whole.classList.add('tailVertical');
+    } else if (i === len - 1 && orientation === 'V') {
+      whole.classList.add('headVertical');
+    } else if (orientation === 'V') {
+      whole.classList.add('verticalMiddle');
+    }
+
+    const ship = document.createElement('div');
+
+    whole.appendChild(ship);
+
+    ship.classList.add('hitmark');
   }
 }
