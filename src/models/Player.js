82bdf772b15106568d80eafd1847dfaa885a3b7f
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-29 02:53:26 +0800

    refactor: improve code and readability and remove duplacation

--- src/models/Player.js
+++ src/models/Player.js
@@ -2,29 +2,27 @@ import { Gameboard } from './Gameboard.js';
 import { Ship } from './Ship.js';
 
 export class Player {
-  constructor(name = 'Player') {
+  constructor(name = 'Computer') {
     this.name = name;
     this.gameboard = new Gameboard();
-    this.ships = [
-      new Ship(5),
-      new Ship(4),
-      new Ship(3),
-      new Ship(3),
-      new Ship(2),
-    ];
+    this.ships = this.createShips();
   }
 
   resetGameboard() {
     this.gameboard = new Gameboard();
   }
 
-  resetShipDamage() {
-    this.ships = [
-      new Ship(5),
-      new Ship(4),
-      new Ship(3),
-      new Ship(3),
-      new Ship(2),
+  createShips() {
+    return [
+      new Ship(5, 'carrier'),
+      new Ship(4, 'battleship'),
+      new Ship(3, 'destroyer'),
+      new Ship(3, 'submarine'),
+      new Ship(2, 'patrolBoat'),
     ];
+  }
+
+  resetShipDamage() {
+    this.ships = this.createShips();
   }
 }
