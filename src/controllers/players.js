c66aeb1d2038c9048199e16ff80c6822d2aaf530
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-29 02:59:52 +0800

    refactor: move  render  on index

--- src/controllers/players.js
+++ src/controllers/players.js
@@ -1,11 +1,7 @@
-import { renderBoard } from '../render/render-board.js';
-
 import { Player } from '../models/Player.js';
 
 export const P1Element = document.querySelector('.player1');
 export const P2Element = document.querySelector('.player2');
 
-export const player1 = new Player();
-export const player2 = new Player('Computer');
-
-renderBoard(player1, P1Element);
+export const player1 = new Player('Kinth');
+export const player2 = new Player();
